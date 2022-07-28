926f2910d8b42b43595d4f0d409944d9ac94a3d6
lucas santos <kelytonlucas@gmail.com>
2022-07-28 16:06:04 -0300

    Project created

---
+++ src/main/java/com/projectBasic/JVBasicCourse/JvBasicCourseApplication.java
@@ -0,0 +1,13 @@
+package com.projectBasic.JVBasicCourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JvBasicCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JvBasicCourseApplication.class, args);
+	}
+
+}
