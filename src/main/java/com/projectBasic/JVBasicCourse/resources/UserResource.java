e2e33b24ae26c8b49424014424e6d042b2d2b19f
lucas santos <kelytonlucas@gmail.com>
2022-07-28 18:34:24 -0300

    User entity and resource

---
+++ src/main/java/com/projectBasic/JVBasicCourse/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.projectBasic.JVBasicCourse.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.projectBasic.JVBasicCourse.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L,"Maria","m@gmail.com","98022039","123456");
+        return ResponseEntity.ok().body(u);
+    }
+}
