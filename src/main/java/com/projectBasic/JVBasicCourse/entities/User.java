cfcfa3528d86bc75da217149ea2729f6dda0a411
lucas santos <kelytonlucas@gmail.com>
2022-07-30 11:41:00 -0300

    Postgres, Jpa

--- src/main/java/com/projectBasic/JVBasicCourse/entities/User.java
+++ src/main/java/com/projectBasic/JVBasicCourse/entities/User.java
@@ -2,10 +2,20 @@ package com.projectBasic.JVBasicCourse.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TB_JVBASIC")
 public class User implements Serializable{
 //serializable transforma meus objetos em cadeias de bytes (assim pode trafegar na rede, ser gravado em arquivo, etc)
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
