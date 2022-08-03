7d7aff4fd09965ce65575f05e84dab4e75fc9ab2
lucas santos <kelytonlucas@gmail.com>
2022-08-03 13:46:11 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/projectBasic/JVBasicCourse/config/TestConfig.java
+++ src/main/java/com/projectBasic/JVBasicCourse/config/TestConfig.java
@@ -51,6 +51,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);//estou adicionando p1 a categoria 2
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Maria","m@gmail.com","9888888","158538");
         User u2 = new User(null, "joao","t@gmail.com","4863524","657324");
         User u3 = new User(null, "lucas","s@gmail.com","542186354","683574");
