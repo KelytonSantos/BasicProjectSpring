99473c37a492fd87fb73c14d7dc18e48faa79143
lucas santos <kelytonlucas@gmail.com>
2022-08-04 18:26:06 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/projectBasic/JVBasicCourse/entities/Product.java
+++ src/main/java/com/projectBasic/JVBasicCourse/entities/Product.java
@@ -11,8 +11,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 // import javax.persistence.Transient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -33,6 +36,10 @@ public class Product implements Serializable {
     joinColumns = @JoinColumn(name = "product_id"),
     inverseJoinColumns = @JoinColumn(name = "category_id"))//@Joincolumn = chave estrangeira do produto | inverseJoinColumn = chave estrang da outra entity
     private Set<Category> categories = new HashSet<>();
+
+    //Não admite repetições do mesmo item
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product(){
     }
@@ -87,6 +94,15 @@ public class Product implements Serializable {
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items){ //percorro a coleção
+            set.add(x.getOrder()); //adiciono no meu conjunto o x.getOrder(este esta associado ao OrdemItem)
+        }
+        return set;
     }
 
     @Override
