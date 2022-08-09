df952147983c7314f23b7c9d08f6e94b091739fa
lucas santos <kelytonlucas@gmail.com>
2022-08-08 21:30:49 -0300

    User Delete

--- src/main/java/com/projectBasic/JVBasicCourse/resources/UserResource.java
+++ src/main/java/com/projectBasic/JVBasicCourse/resources/UserResource.java
@@ -1,9 +1,10 @@
 package com.projectBasic.JVBasicCourse.resources;
-import java.net.URI;
+import java.net.URI; 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -39,8 +40,14 @@ public class UserResource {
 	public ResponseEntity<User> isert(@RequestBody User obj) {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-		.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		.path("/{id}").buildAndExpand(obj.getId()).toUri(); //chamando método para criação de post no padrão correto
 		return ResponseEntity.created(uri).body(obj);
+	}
+
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 }
